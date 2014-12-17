4051c84a0c8fda1d206333e76ca398d640d15ac1
root <root@raspberrypi.(none)>
2014-12-17 20:48:41 +0000

    added export fix

--- server.js
+++ server.js
@@ -25,5 +25,16 @@ app.get('/', mainPage);
 app.get('/switch/*', switcher);
 app.get('/status', status);
 
-app.listen(3000);
+app.listen(80, "192.168.2.200");
 console.log("webserver ready...");
+
+//export GPIO pins
+var exec = require("child_process").exec;
+exec("sudo echo 22 > /sys/class/gpio/export", function (error, stdout, stderr) {
+	console.log(stdout.trim());
+	console.log(stderr.trim());
+});
+exec("sudo echo 23 > /sys/class/gpio/export", function (error, stdout, stderr) {
+	console.log(stdout.trim());
+	console.log(stderr.trim());
+});
